48a3874a8b50c57ade6d0f5f4ac770e22cc60ac9
M. Gorshkov <mgorshkov@tle.one>
2021-01-26 14:37:32 +0300

    Minibatch 2

---
+++ net.py
@@ -0,0 +1,123 @@
+import numpy as np
+from collections import OrderedDict
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from custom import *
+from utils import *
+
+class Generator(nn.Module):
+    def __init__(self, properties):
+        super(Generator, self).__init__()
+        # initialize parameters
+        self.P = properties
+
+        # initialize network
+        self.layers = []
+        # first section
+        self.layers.extend([ 
+            ConvBlock(self.P.ch_in, self.P.ch_in, 2**self.P.p_min, 1, 0), 
+            ConvBlock(self.P.ch_in, self.P.ch_in, 3, 1, 1)
+        ])
+
+        # middle section 
+        for i in range(len(self.P.ch_pr)):
+            self.layers.extend([
+                nn.Upsample(scale_factor=2, mode='nearest'),
+
+                ConvBlock(self.P.ch_in, self.P.ch_pr[i], 3, 1, 1) if i == 0 else \
+                ConvBlock(self.P.ch_pr[i-1], self.P.ch_pr[i], 3, 1, 1), 
+
+                ConvBlock(self.P.ch_pr[i], self.P.ch_pr[i], 3, 1, 1)
+            ])
+
+        # last section 
+        self.layers.append(ToImage(self.P.ch_pr[-1], self.P.ch_image))
+
+        self.Sequential = nn.Sequential(*self.layers)
+
+
+
+    def forward(self, x_b):
+        # standard, exept the case when the P value changes (then grow)
+        return self.Sequential(x_b)
+
+    def grow(self):
+        # create a new model with identical modules, copy state_dict of each old module to the new model
+        # delete the old model
+        # add new layers and initialize them
+        pass
+
+
+class Discriminator(nn.Module):
+    def __init__(self, properties):
+        super(Discriminator, self).__init__()
+        # initialize parameters
+        self.P = properties
+
+        # initialize network
+        self.layers = []
+        # first section
+        self.layers.append(FromImage(self.P.ch_image, self.P.ch_pr[0]))
+
+        # middle section 
+        for i in range(len(self.P.ch_pr)):
+            self.layers.extend([
+                ConvBlock(self.P.ch_pr[i], self.P.ch_pr[i], 3, 1, 1, False), 
+
+                ConvBlock(self.P.ch_pr[i], self.P.ch_out, 3, 1, 1, False) if i == len(self.P.ch_pr)-1 else \
+                ConvBlock(self.P.ch_pr[i], self.P.ch_pr[i+1], 3, 1, 1, False),
+
+                nn.MaxPool2d(2)
+            ])
+
+        # last section 
+        self.layers.extend([ 
+            Minibatch(),
+            ConvBlock(self.P.ch_out+1, self.P.ch_out, 3, 1, 1, False), 
+            nn.Conv2d(self.P.ch_out, self.P.ch_out, 2**self.P.p_min, 1, 0),
+
+            nn.Flatten(),   
+            nn.Linear(self.P.ch_out, 1)
+        ])
+
+        self.Sequential = nn.Sequential(*self.layers)
+
+
+
+    def forward(self, x_b):
+        # standard, exept the case when the P value changes (then grow)
+        return self.Sequential(x_b)
+
+    def grow(self):
+        # create a new model with identical modules, copy state_dict of each old module to the new model
+        # delete the old model
+        # add new layers and initialize them
+        pass
+
+
+
+
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# P = Properties()
+
+# # device = torch.device('cpu')
+
+# D = Discriminator(P.D)
+# print(sum(p.numel() for p in D.parameters() if p.requires_grad))
+# to_device(D, device)
+# t = torch.randn(100, 1, 32, 32).to(device)
+
+# print(D(t).shape)
+
+# G = Generator(P.G)
+# print(sum(p.numel() for p in G.parameters() if p.requires_grad))
+# to_device(G, device)
+# t = torch.randn(1, 512, 1, 1).to(device)
+
+# print(G(t).shape)
+
+# print(ConvBlock(32, 32, 4, 2, 1, Deconv=False)(torch.randn(1,32,16,16)).shape)
---
+++ trainer.py
@@ -0,0 +1,126 @@
+import numpy as np
+from collections import OrderedDict
+import math
+import os
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader,  Dataset
+import torch.nn.functional as F
+
+import torchvision
+from torchvision.transforms import ToTensor, Normalize, Compose, Pad
+from torchvision.datasets import MNIST
+import torchvision.transforms as tt
+from torchvision.utils import save_image
+from torchvision.datasets.utils import download_url
+
+from demo import *
+from custom import *
+from utils import *
+from net import *
+
+class PGAN:
+    def __init__(self, properties, data_loader):
+        self.initialize_network(properties)
+        self.initialize_data(data_loader)
+        
+
+    def initialize_network(self, properties):
+        self.P = properties
+
+        self.G = Generator(self.P.G)
+        self.D = Discriminator(self.P.D)
+
+        self.G.to(self.P.device)
+        self.D.to(self.P.device)
+
+        self.G_opt = torch.optim.RMSprop(self.G.parameters(), lr=self.P.G.lr, momentum=5e-5)
+        self.D_opt = torch.optim.RMSprop(self.D.parameters(), lr=self.P.D.lr, momentum=5e-5)
+
+    def initialize_data(self, data_loader):
+        self.DL = data_loader
+        self.batch_size = self.DL.batch_size
+        self.batches_number = len(self.DL)
+
+    def reset_grad(self):
+        self.G_opt.zero_grad()
+        self.D_opt.zero_grad()
+
+    def train_D(self, batch):
+        real_labels = torch.empty(len(batch), 1).fill_(.9).to(self.P.device)
+        fake_labels = torch.zeros(len(batch), 1).to(self.P.device)
+
+        # Loss for real images
+        outputs = self.D(batch)
+        D_loss_real = self.P.loss(outputs, real_labels)
+        real_score = outputs
+
+        # Loss for fake images
+        z = torch.randn(len(batch), self.P.latent, 1, 1).to(self.P.device)
+        fake_images = self.G(z)
+        outputs = self.D(fake_images)
+        D_loss_fake = self.P.loss(outputs, fake_labels)
+        fake_score = outputs
+
+        # Backprop and optimize
+        D_loss = D_loss_real + D_loss_fake
+        
+        self.reset_grad()
+        D_loss.backward()
+        self.D_opt.step()
+
+        return D_loss, real_score, fake_score
+
+    def train_G(self):
+        # Generate fake images and calculate loss
+        z = torch.randn(self.batch_size, self.P.latent, 1, 1).to(self.P.device)
+        fake_images = self.G(z)
+
+        labels = torch.ones(self.batch_size, 1).to(self.P.device)
+        G_loss = self.P.loss(self.D(fake_images), labels)
+
+        # Backprop and optimize
+        self.reset_grad()
+        G_loss.backward()
+        self.G_opt.step()
+
+        return G_loss, fake_images
+
+    def fit(self, epochs):
+        d_losses, g_losses, real_scores, fake_scores = [], [], [], []
+
+        for epoch in range(epochs):
+            for i, (images, _) in enumerate(self.DL):
+                images = images.to(self.P.device)
+                
+                D_loss, real_score, fake_score = self.train_D(images)
+                G_loss, fake_images = self.train_G()
+                # print(i)
+
+            # pgan_demo.refresh(epoch)
+
+            print(f"""Epoch {epoch}:
+            Discriminator loss: {D_loss}; Real score: {real_score.mean().item()}; Fake score{fake_score.mean().item()};
+            Generator loss: {G_loss}""")
+
+            self.demo()
+
+    def demo(self, size=6):
+        with torch.no_grad():
+            latent = torch.randn(size**2, self.P.G.ch_in, 1, 1).to(self.P.device)
+            images = self.G(latent).clone().detach()
+            images = images.reshape(images.size(0), self.P.G.ch_image, 2**self.P.G.p_max, 2**self.P.G.p_max)
+            
+            directory = 'images'
+                
+            images = denorm(images)
+            save_image(images, os.path.join(directory, 'demo.png'), nrow=size)
+            
+P = Properties()
+# P.D.lr = 5e-7
+DL = mnist_get_data(P.device, 100)
+
+pgan = PGAN(P, DL)
+pgan.fit(10)
+
